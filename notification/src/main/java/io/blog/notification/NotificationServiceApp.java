25d54c21358322cc31c66778a2a84d6f399b8f23
devflu999 <pmnAQWespagnol10>
2025-08-31 10:15:00 +0100

    feat(notification): setup RabbitMQ consumer and notification handling

---
+++ notification/src/main/java/io/blog/notification/NotificationServiceApp.java
@@ -0,0 +1,22 @@
+
+package io.blog.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "io.blog.notification",
+                "io.message.server",
+        }
+)
+public class NotificationServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApp.class, args);
+    }
+
+
+
+
+
+}
